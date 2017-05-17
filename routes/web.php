172edf313484f4b7a53f2a3560a9ccf35f1512b6
minhtrang <trangbtm31@gmail.com>
2017-05-17 11:29:05 +0700

    #005 Build Timeline page

--- routes/web.php
+++ routes/web.php
@@ -40,5 +40,7 @@ Route::get('/dislike-ajax', ['as' => 'dislike-ajax', 'uses' => 'HomeController@D
 Route::get('/admin_area', ['middleware' => 'admin', function () {
      return view('welcome');
 }]);
+
 Route::post('/create-plan', ['as' => 'create-plan', 'uses' => 'Plan\PlanController@create']);;
-Route::post('/post-comment/{plan_id}', ['as' => 'post-comment', 'uses' => 'Plan\PlanController@postComment']);Route::post('/create-plan', ['as' => 'create-plan', 'uses' => 'Plan\PlanController@create' ]);
+Route::post('/post-comment/{plan_id}', ['as' => 'post-comment', 'uses' => 'Plan\PlanController@postComment']);
+Route::post('/create-plan', ['as' => 'create-plan', 'uses' => 'Plan\PlanController@create' ]);
